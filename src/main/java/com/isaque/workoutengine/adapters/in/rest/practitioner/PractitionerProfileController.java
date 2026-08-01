6317020b4e0c63a127cc6a08fc9a0eeccf07d992
isaquemaestri <isaquemaestri01@gmail.com>
2026-08-01 17:29:51 -0300

    feat: add controller for post router

---
+++ src/main/java/com/isaque/workoutengine/adapters/in/rest/practitioner/PractitionerProfileController.java
@@ -0,0 +1,50 @@
+package com.isaque.workoutengine.adapters.in.rest.practitioner;
+
+import com.isaque.workoutengine.application.practitioner.RegisterPractitionerProfileDto;
+import com.isaque.workoutengine.application.practitioner.RegisterPractitionerProfileService;
+import com.isaque.workoutengine.domain.practitioner.PractitionerProfile;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+
+@Controller("/practitioner-profiles")
+public class PractitionerProfileController {
+
+	private final RegisterPractitionerProfileService registerPractitionerProfileService;
+
+	public PractitionerProfileController(RegisterPractitionerProfileService registerPractitionerProfileService) {
+		this.registerPractitionerProfileService = registerPractitionerProfileService;
+	}
+
+	@Post
+	public HttpResponse<PractitionerProfileResponseDto> register(RegisterPractitionerProfileRequestDto requestDto) {
+		var dto = toApplicationDto(requestDto);
+		var profile = registerPractitionerProfileService.register(dto);
+
+		return HttpResponse.created(toResponseDto(profile));
+	}
+
+	private RegisterPractitionerProfileDto toApplicationDto(RegisterPractitionerProfileRequestDto requestDto) {
+		return new RegisterPractitionerProfileDto(
+						requestDto.dateOfBirth(),
+						requestDto.heightCm(),
+						requestDto.weightKg(),
+						requestDto.experienceLevel(),
+						requestDto.trainingGoal(),
+						requestDto.weeklyFrequency()
+		);
+	}
+
+	private PractitionerProfileResponseDto toResponseDto(PractitionerProfile profile) {
+		return new PractitionerProfileResponseDto(
+						profile.id(),
+						profile.dateOfBirth(),
+						profile.ageToday(),
+						profile.heightCm(),
+						profile.weightKg(),
+						profile.experienceLevel(),
+						profile.trainingGoal(),
+						profile.weeklyFrequency()
+		);
+	}
+}
